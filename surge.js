fbc507a56abb42bdc06c6a97699d7ed52490d017
daz4126 <daz4126@gmail.com>
2024-04-27 10:11:09 +0100

    v3 - made reactive-value

--- surge.js
+++ surge.js
@@ -1,10 +1,35 @@
 function surge(actions={}){
-    const actionElements = [...document.querySelectorAll("[data-surge] [data-action]")]
+    const actionElements = [...document.querySelectorAll("[data-surge] [data-action]")]    
     const elements = [...document.querySelectorAll("[data-surge] [id]")].reduce((obj,el) => {
-        Object.entries(el.dataset).forEach(([key,value]) => {
+       Object.entries(el.dataset).forEach(([key,value]) => {
+        if(key !== "action" && key !== "reactiveValue"){
           Object.defineProperty(el, key, {
+                get: function() { 
+                  const val = el.dataset[key]
+                  try {
+                    return JSON.parse(val)
+                  } catch (e) {
+                    return val
+                  }
+                },
+                set: function(value) {
+                  try {
+                    JSON.parse(value)
+                    el.setAttribute("data-"+key,JSON.stringify(value))
+                  } catch (e) {
+                     el.setAttribute("data-"+key,value)
+                  }
+                }
+          }) // end Object.defineProperty
+        } // end if
+      }) // end Object.entries.forEach
+      // add reactive value
+        if(el.dataset.reactiveValue !== undefined) {
+          // set reactive value text content
+          el.textContent = el.dataset.reactiveValue
+          Object.defineProperty(el, "value", {
             get: function() { 
-            const val = this.dataset[key]
+            const val = this.dataset.reactiveValue
             try {
                     return JSON.parse(val)
                 } catch (e) {
@@ -14,21 +39,19 @@ function surge(actions={}){
             set: function(value) {
             try {
                     JSON.parse(value)
-                    this.setAttribute("data-"+key,JSON.stringify(value))
+                    this.setAttribute("data-reactive-value",JSON.stringify(value))
                 } catch (e) {
-                    this.setAttribute("data-"+key,value)
+                    this.setAttribute("data-reactive-value",value)
                 }
-            if(this.dataset.reactive !== undefined && key === "value"){
                 this.textContent = value
             }
-        }
-        })
-        if(el.dataset.reactive !== undefined){
-            el.textContent = el.value
-        }
-     })
-        return {...obj,[el.id]: el}
-    },{})
+        }) // end objectDefineProperty
+      } // end if
+        
+      // add id references
+      return {...obj,[el.id]: el}
+    },{}) // end of reduce
+    
         
     if(actions.connect){
         actions.connect(elements)
